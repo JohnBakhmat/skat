1a0e7c0b9fe2a3265835e1f21c411715aac6893e
John Bakhmat <johnbakhmat@gmail.com>
2025-02-12 17:58:00 +0300

    fix: city block

--- src/components/city-block.tsx
+++ src/components/city-block.tsx
@@ -1,4 +1,4 @@
-// import MapPin from "../assets/map-pin.svg";
+import { MapPin } from "./map-pin";
 interface Props {
 	cities: string[];
 }
@@ -6,7 +6,8 @@ interface Props {
 export const CityBlock = ({ cities }: Props) => (
 	<div className="flex flex-col justify-end">
 		<div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] place-items-center gap-y-5 gap-x-10 pb-5">
-			{/* <MapPin class="size-10" /> */}
+			<MapPin className="size-10" />
+
 			<div className="flex flex-col gap-y-2 row-span-2 justify-self-start text-4xl">
 				{cities.map((x) => (
 					<p key={x}>{x}</p>
