277f17c05124ea78680a9f3f00a067b5fa2566bd
John Bakhmat <johnbakhmat@gmail.com>
2025-03-03 18:56:03 +0300

    🚀 refactor: update text content and styles for WhyBlock component

--- src/components/why-block.tsx
+++ src/components/why-block.tsx
@@ -31,19 +31,23 @@ export default function WhyBlock() {
 		<>
 			<WhyItem
 				idx="01"
-				text={"С нами можно\nбыть спокойным за проект"}
+				text={
+					"С нами можно быть уверенными \n в успешной реализации \n мероприятия"
+				}
 				isActive={lastHovered === 0 && !isMobile}
 				onHover={() => handleHover(0)}
 			/>
 			<WhyItem
 				idx="02"
-				text={"Мы заряжаем своей\nэнергией всех вокруг"}
+				text={
+					"Мы передаем свое спокойствие \n всей команде проекта, как \n следствие – реализация без нервов \n и намека на хаос"
+				}
 				isActive={lastHovered === 1 && !isMobile}
 				onHover={() => handleHover(1)}
 			/>
 			<WhyItem
 				idx="03"
-				text={"Мы знаем все течения\nи куда нужно плыть"}
+				text={"Мы знаем все течения и куда \nнужно плыть"}
 				isActive={lastHovered === 2 && !isMobile}
 				onHover={() => handleHover(2)}
 			/>
