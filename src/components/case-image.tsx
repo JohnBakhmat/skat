2b7152dd58a3122606c520e6c9ada5f7b5f7b506
John Bakhmat <johnbakhmat@gmail.com>
2025-03-04 13:02:52 +0300

    🎨 chore: format code for consistency and readability in files

--- src/components/case-image.tsx
+++ src/components/case-image.tsx
@@ -1,20 +1,18 @@
 interface Props {
-	src?: string;
-	className?: string;
-	imageClassName?: string;
+  src?: string;
+  className?: string;
+  imageClassName?: string;
 }
 
 export const CaseImage = ({ src, className, imageClassName }: Props) => (
-	<div
-		className={`rounded-[50px] overflow-hidden shadow-lg h-full w-full ${className}`}
-	>
-		<img
-			className={`w-full h-full object-cover ${imageClassName}`}
-			alt="case"
-			src={
-				src ??
-				"https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
-			}
-		/>
-	</div>
+  <div
+    className={`rounded-[50px] overflow-hidden shadow-lg h-full w-full ${className}`}
+  >
+    <img
+      className={`w-full h-full object-cover ${imageClassName}`}
+      alt="case"
+      src={src ??
+        "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"}
+    />
+  </div>
 );
